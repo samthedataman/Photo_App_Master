fab7330c9d846262b5bb61d68bdfd1c90cc94e94
Sam Savage <80926633+samthedataman@users.noreply.github.com>
2022-11-27 01:49:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from PIL.ExifTags import TAGS
 from pathlib import Path
 import os
 from PIL import ImageEnhance
-import oauth2client
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 from os import listdir
